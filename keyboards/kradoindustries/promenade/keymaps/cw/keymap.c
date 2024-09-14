19dce864e8350b75f6b8a5cdf3d1755e7f1c5435
Claire West <bokthaku@gmail.com>
2024-09-14 15:18:50 -0400

    Promenade layout

--- keyboards/kradoindustries/promenade/keymaps/cw/keymap.c
+++ keyboards/kradoindustries/promenade/keymaps/cw/keymap.c
@@ -69,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * .-----------------------------------------------------------------------------------------------------------------------------.
      * | ESC    | 1      | 2      | 3      | 4      | 5      | -      | =      | 6      | 7      | 8      | 9      | 0      | BACKSP |
      * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
-     * | TAB    | Q      | W      | F      | P      | B      | [      | ]      | J      | P      | U      | Y      | ;      | \      |
+     * | TAB    | Q      | W      | F      | P      | B      | [      | ]      | J      | L      | U      | Y      | ;      | \      |
      * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
      * | F5     | A      | R      | S      | T      | G      |        |        | M      | N      | E      | I      | O      | '      |
      * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
@@ -80,9 +80,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     [_CMK] = LAYOUT_all(
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS, KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-        KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_LBRC, KC_RBRC, KC_J,    KC_P,    KC_U,    KC_Y,    KC_SCLN, KC_BSLS,
+        KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_LBRC, KC_RBRC, KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSLS,
         KC_F5,   KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                      KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_DEL,  KC_BSPC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_DEL,  KC_BSPC, KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
         KC_LCTL, KC_LGUI, KC_LALT, MO(_FN), KC_GRV,  KC_ENT,      KC_SPC,       KC_SPC,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, TO(_BASE)
     )
 };
